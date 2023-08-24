d897d57f5d2ae6578d7f9de3dabfcb313045f913
Karthik-6362 <137412032+Karthik-6362@users.noreply.github.com>
2023-08-24 20:18:00 +0530

    Delete DAY1/Finding range of signed and unsigned numbers directory

--- DAY1/Finding range of signed and unsigned numbers/Finding range of unsigned long long int.c
+++
@@ -1,9 +0,0 @@
-#include 
-#include 
-int main(){
-	unsigned long long int max = (unsigned long long int) (pow(2,64) -1);
-	unsigned long long int min = (unsigned long long int) (pow(2,64) *(-1));
-	printf("Minimum value is %llu\n",min);
-	printf("Maximum value is %llu\n",max);
-	return 0;
-}
